e012c606f60db321ff5b048ea227251de1d1b135
DaGree <mr.dan6886@yandex.ru>
2021-08-26 20:43:23 +0300

    flutter_demo added images instead of permanent color in first column

--- fft_sound_pack/lib/main.dart
+++ fft_sound_pack/lib/main.dart
@@ -12,15 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'FFT Sound Pack'),
@@ -30,18 +21,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -49,25 +29,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      // _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       backgroundColor: Colors.lightBlue[50],
       endDrawer: Drawer(),
@@ -99,8 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     width: 100.0,
                     height: 100.0,
                     alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                    ),
+                    decoration: BoxDecoration(),
                     child: Image.asset("assets/images/DaGree_Anime.png"),
                   ),
                 ),
@@ -110,15 +79,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(15))),
                 //margin: EdgeInsetsGeometry.infinity,
-                color: Colors.yellowAccent,
-                child: Container(
-                  width: 100.0,
-                  height: 100.0,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "2",
-                    style: TextStyle(fontSize: 24.0, color: Colors.black),
-                    textAlign: TextAlign.center,
+                color: Colors.white,
+                child: GestureDetector(
+                  onTap: () {
+                    print("Card 2 is active");
+                  },
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    alignment: Alignment.center,
+                    child: Image.asset("assets/images/Misha_kid.png"),
                   ),
                 ),
               ),
@@ -127,15 +97,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(15))),
                 //margin: EdgeInsetsGeometry.infinity,
-                color: Colors.yellowAccent,
-                child: Container(
-                  width: 100.0,
-                  height: 100.0,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "3",
-                    style: TextStyle(fontSize: 24.0, color: Colors.black),
-                    textAlign: TextAlign.center,
+                color: Colors.white,
+                child: GestureDetector(
+                  onTap: () {
+                    print("Card 3 is active");
+                  },
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    alignment: Alignment.center,
+                    child: Image.asset("assets/images/Xomyak_brutal.png"),
                   ),
                 ),
               ),
@@ -144,15 +115,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(15))),
                 //margin: EdgeInsetsGeometry.infinity,
-                color: Colors.yellowAccent,
-                child: Container(
-                  width: 100.0,
-                  height: 100.0,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "4",
-                    style: TextStyle(fontSize: 24.0, color: Colors.black),
-                    textAlign: TextAlign.center,
+                color: Colors.white,
+                child: GestureDetector(
+                  onTap: () {
+                    print("Card 4 is active");
+                  },
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    alignment: Alignment.center,
+                    child: Image.asset("assets/images/zwitter_danger.png"),
                   ),
                 ),
               ),
@@ -161,15 +133,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(15))),
                 //margin: EdgeInsetsGeometry.infinity,
-                color: Colors.yellowAccent,
-                child: Container(
-                  width: 100.0,
-                  height: 100.0,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "5",
-                    style: TextStyle(fontSize: 24.0, color: Colors.black),
-                    textAlign: TextAlign.center,
+                color: Colors.white,
+                child: GestureDetector(
+                  onTap: () {
+                    print("Card 5 is active");
+                  },
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    alignment: Alignment.center,
+                    child: Image.asset("assets/images/Decoy_what.png"),
                   ),
                 ),
               ),
