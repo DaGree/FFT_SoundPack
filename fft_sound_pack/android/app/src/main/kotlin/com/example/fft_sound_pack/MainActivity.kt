cf3319425c592e4da2a0181920ec150b88697de4
DaGree <mr.dan6886@yandex.ru>
2021-09-05 12:30:55 +0300

    adding all cases of sound

--- fft_sound_pack/android/app/src/main/kotlin/com/example/fft_sound_pack/MainActivity.kt
+++ fft_sound_pack/android/app/src/main/kotlin/com/example/fft_sound_pack/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.fft_sound_pack
+package dagree.fft_sound_pack
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
 }
